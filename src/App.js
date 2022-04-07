0b0ed33ff681b9317592cdfd500f1b2d384335f6
sovchinnikov <owchin.s@yandex.ru>
2022-04-07 15:17:03 +0300

    Создал модальное окно для внесение новых записей

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
+import ModalWindow from "./components/Modal/ModalWindow";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -10,13 +11,18 @@ function App() {
         {id: 4, title: "Первый пост", date: "10.04.2000", status: "В процессе"},
         {id: 5, title: "Первый пост", date: "10.04.2000", status: "В процессе"},
     ])
+    const [modal, setModal] = useState(false)
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+
     return (
         <div className="app">
-            <PostList postList={posts} name={"serg"} remove={removePost}/>
+            <ModalWindow visible={modal} setVisible={setModal}>
+
+            </ModalWindow>
+            <PostList postList={posts} name={"serg"} remove={removePost} addControl={setModal}/>
             <div className="viewer">hello</div>
         </div>
     );
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PostItem from "./PostItem";
 
-const PostList = ({postList, name, remove}) => {
+const PostList = ({postList, name, remove, addControl}) => {
     return (
         <div className="post_list">
             <div className="post_list_header">
                 <h1>Список оптимизаций пользователя {name}</h1>
             </div>
-            <button className="post_list_add_btn">Add post</button>
+            <button onClick={() => addControl(true)} className="post_list_add_btn">Add post</button>
             {
                 postList.map((post) =>
                     <PostItem post={post} key={post.id} remove={remove}/>
