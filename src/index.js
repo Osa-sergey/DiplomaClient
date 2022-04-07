b4ab83df5479c21a3082fddf0b59ff7939645a73
sovchinnikov <owchin.s@yandex.ru>
2022-04-07 16:28:54 +0300

    Поправка для убирания предупреждений при удалении элементов

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 const app = (
-    <StrictMode>
         <App />
-    </StrictMode>
 );
 
 root.render(app);
