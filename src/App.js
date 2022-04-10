4767b27cff05c9a961e1bc407be03c8a47c5f55d
sovchinnikov <owchin.s@yandex.ru>
2022-04-10 18:40:53 +0300

    Перегрупперовка элементов интерфейса. Все что принадлежит postList теперь лежит в postList. Добавлена кнопка update для листа постов

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import ModalWindow from "./components/Modal/ModalWindow";
 import LoginForm from "./components/LoginForm";
 import {Context} from "./index";
 import {observer} from "mobx-react-lite"
-import CreateNewPost from "./components/CreateNewPost";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
-        {id: 2, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
-        {id: 3, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
-        {id: 4, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
-        {id: 5, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
-    ])
     const [loginModal, setLoginModal] = useState(false)
-    const [postModal, setPostModal] = useState(false)
-
     const {store} = useContext(Context)
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
 
     if (!store.isAuth) {
         return (
@@ -34,11 +20,10 @@ function App() {
 
     return (
         <div className="app">
-            <CreateNewPost postModal={postModal} setPostModal={setPostModal}/>
             <ModalWindow visible={loginModal} setVisible={setLoginModal}>
                 <LoginForm setModalVisible={setLoginModal}/>
             </ModalWindow>
-            <PostList postList={posts} name={store.user} remove={removePost} addControl={setPostModal}/>
+            <PostList/>
             <button className="logout_button" onClick={() => store.logout()}>logout</button>
             <div className="viewer">hello</div>
         </div>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import PostItem from "./PostItem";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
+import {Context} from "../index";
+import {observer} from "mobx-react-lite";
+import CreateNewPost from "./CreateNewPost";
 
-const PostList = ({postList, name, remove, addControl}) => {
+const PostList = () => {
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
+        {id: 2, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
+        {id: 3, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
+        {id: 4, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
+        {id: 5, title: "Первый пост", date: "10.04.2000", status: "В процессе", bs_number: 5},
+    ])
+    const [postModal, setPostModal] = useState(false)
+    const {store} = useContext(Context)
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    function updatePostList() {
+
+    }
+
     return (
-        <div className="post_list">
-            <div className="post_list_header">
-                <h1>Список оптимизаций пользователя {name}</h1>
+        <div>
+            <CreateNewPost postModal={postModal} setPostModal={setPostModal}/>
+            <div className="post_list">
+                <div className="post_list_header">
+                    <h1>Список оптимизаций пользователя {store.user}</h1>
+                </div>
+                <div className="post_list_btns">
+                    <button onClick={() => setPostModal(true)}>Add post</button>
+                    <button onClick={() => updatePostList()}>Update</button>
+                </div>
+                <TransitionGroup>
+                    {
+                        posts.map((post) =>
+                            <CSSTransition
+                                key={post.id}
+                                timeout={500}
+                                classNames="post">
+                                <PostItem post={post} remove={removePost}/>
+                            </CSSTransition>
+                        )
+                    }
+                </TransitionGroup>
             </div>
-            <button onClick={() => addControl(true)} className="post_list_add_btn">Add post</button>
-            <TransitionGroup>
-                {
-                    postList.map((post) =>
-                        <CSSTransition
-                            key={post.id}
-                            timeout={500}
-                            classNames="post">
-                            <PostItem post={post} remove={remove}/>
-                        </CSSTransition>
-                    )
-                }
-            </TransitionGroup>
         </div>
     );
 };
 
-export default PostList;
+export default observer(PostList);
