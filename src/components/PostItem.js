2678a173c7f869fcb3b5c6bbadf1b75f02f0eb4c
sovchinnikov <owchin.s@yandex.ru>
2022-04-11 14:52:13 +0300

    Сделан корректный вывод даты оптимизации

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const PostItem = ({post, remove, select}) => {
+    const date = new Date(post.date);
+    const datetime = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()<10 ? '0': ''}${date.getMinutes()}`;
     return (
         <div onClick={() => select(post)} className="post">
             <div className="post_content">
                 <h1>
                     <strong className="header_text">{post.title}</strong>
                 </h1>
-                <h3><b>Дата оптимизации:</b> {post.date}</h3>
+                <h3><b>Дата оптимизации:</b> {datetime}</h3>
                 <h3><b>Статус:</b> {post.status}</h3>
                 <h3><b>Кол-во БС:</b> {post.bs_number}</h3>
             </div>
